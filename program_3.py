74610d81db35ab53b18943495eb23c7fda2f0617
Sabria-F <sjfafach@students.unwsp.edu>
2025-03-28 15:28:04 -0500

    Program3AverageNumbersSabriaFafach

--- program_3.py
+++ program_3.py
@@ -1,18 +1,25 @@
-# Program #3: Average Numbers
-# Assume a file containing a series of integers is named numbers.txt and exists on the computer's disk.
-# (please use the provided numbers.txt)
-# Write a program that reads all of the numbers stored in the file and calculates their total.  
+#By:Sabria Fafach
+#Date: March 28, 2025
+#Title:program_3.py
 
-# The program should handle the following exceptions: 
 
-# It should handle any IOError exceptions that are raised.
-# It should handle any ValueError exceptions that are raised when the items that are read from the file 
-# are converted to a number.
 def sum_numbers_from_file():
-    ######################
-    # Add your code here #
-    ######################
-    print('In the sum_numbers_from_file function')
+    try:
+        read_file=open("numbers.txt","r")
+        total_numbers=0
+        line=read_file.readline()
+        while line!="":
+            total_numbers+=float(line.strip())
+            line=read_file.readline()
+        print(f"The total of all the numbers in the numbers.txt file is {total_numbers:.2f}.")
+
+        print('In the sum_numbers_from_file function')
+
+    except IOError:
+        print("Sorry the file you are trying to access does not exist, or a problem occurred while reading text from file.")
+    except ValueError as err:
+        print(err)
+
 
 # You don't need to change anything below this line:
 if __name__ == '__main__':
