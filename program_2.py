291b178a842e9669b3aa2c6f8157f717d6eaef15
Sabria-F <sjfafach@students.unwsp.edu>
2025-03-28 15:00:20 -0500

    Program2RandomNumberFileWriterSabriaFafach

--- program_2.py
+++ program_2.py
@@ -1,5 +1,26 @@
-# Program #2: Random Number File Writer
-# Write a program that writes a series of random numbers (up to 1000) to a file.
-# Each random number should be in the range of 1 through 500. 
-# The application should let the user specify how many random numbers the file will hold 
-# (up to 1000).
+#By:Sabria Fafach
+#Date: March 28, 2025
+#Title:program_2.py
+
+
+import random
+NUMBER_QUANTITY=int(input("Enter the number of random numbers you want to write to the file (up to 1000):"))
+
+def main():
+    r_number_file=open("random_numbers.txt","a")
+    for number in range(0,NUMBER_QUANTITY):
+        random_number=random.randint(1,500)
+        r_number_file.write(f"{random_number}\n")
+
+    r_number_file.close()
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
